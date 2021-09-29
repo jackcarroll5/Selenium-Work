7a96ccb84a12f6eb25cfd26469e202da875e3bde
jackcarroll5 <jackcarroll9000@gmail.com>
2021-09-29 17:41:45 +0100

    Attempt to select the check in calendar date in the calendar table of the Booking website for automation.

--- Booking_Test/src/seleniumbookingtest/Second_Test.java
+++ Booking_Test/src/seleniumbookingtest/Second_Test.java
@@ -37,6 +37,7 @@ public class Second_Test {
 	       
 	    //driver.findElement(By.cssSelector(".bui-calendar__control bui-calendar__control--next")).click();
 	    //driver.findElement(By.cssSelector(".calendar-next")).click();
+	     
 	    
 	    driver.findElement(By.cssSelector("div.bui-calendar__control:nth-child(2)")).click();
 	    driver.findElement(By.cssSelector("div.bui-calendar__control:nth-child(2)")).click();
@@ -48,36 +49,10 @@ public class Second_Test {
 	    Date date = new Date();
 	    String dateToday = dateFormat.format(date);
 	    System.out.println("Today's date is " + dateToday);
-	    
-//	    Calendar cal = Calendar.getInstance();
-//	    SimpleDateFormat month_date = new SimpleDateFormat("MMMM", Locale.UK);
-//	    String month_name = month_date.format(cal.getTime());
-//	    String year = Calendar.getInstance().get(Calendar.YEAR) + "";
-//	    String monthYear = month_name + " " + year;
-//
-//	    // store day
-//	    String day = Calendar.getInstance().get(Calendar.DAY_OF_MONTH) + "";
-//
-//	    // find date
-//	    WebElement date =  driver.findElements(By.xpath("//th[contains(.,'"
-//	        + monthYear + "')]/parent::*/parent::*/parent::*/tbody/tr/td/span[@class = 'bui-calendar__date'][contains(.,'"
-//	        + day + "')]")).get(0);
-//	    date.click();
-	    
-	    // driver.findElement(By.id("jobDate")).click();
-//        Thread.sleep(5000);
-//
-//        Calendar calendar = new GregorianCalendar();
-//
-//        int currentDate = calendar.get(Calendar.DAY_OF_MONTH); // get date field
-//        System.out.println("date is " + currentDate);
-//
-//        driver.findElement(By.xpath("//td[contains(data-date, '2021-12-27')]")).click();
-	        
-	    
+	       
 	    new WebDriverWait(driver, 20).until(ExpectedConditions.elementToBeClickable(By.xpath("//div[contains(@class, 'xp__dates__checkin')]//span[contains(@class, 'calendar-restructure-sb')]"))).click();
-	    new WebDriverWait(driver, 20).until(ExpectedConditions.elementToBeClickable(By.xpath("//div[@class='xp-calendar']//table[@class='bui-calendar__dates']//tr//td[@data-date='2021-12-27']"))).click();
-	    new WebDriverWait(driver, 20).until(ExpectedConditions.elementToBeClickable(By.xpath("//div[@class='xp-calendar']//table[@class='bui-calendar__dates']//tr//td[@data-date='2021-12-28']"))).click();
+	    new WebDriverWait(driver, 20).until(ExpectedConditions.elementToBeClickable(By.xpath("//div[@class='xp-calendar']//table[@class='bui-calendar__dates']//tr//td[@data-date='2021-12-29']"))).click();
+	    new WebDriverWait(driver, 20).until(ExpectedConditions.elementToBeClickable(By.xpath("//div[@class='xp-calendar']//table[@class='bui-calendar__dates']//tr//td[@data-date='2021-12-30']"))).click();
 	    
 	    driver.findElement(By.className("xp__input")).click();  
 	   
